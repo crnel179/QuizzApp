e4867afbd7a178188af2b4d9e643794b8f9ed8ed
Atlan <cl179971@gmail.com>
2020-09-25 13:55:22 +0100

    Quiz App

---
+++ app/src/main/java/claur/quizzapp/MainActivity.java
@@ -0,0 +1,131 @@
+package claur.quizzapp;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO: Declare member variables here:
+    Button mTrueBtn;
+    Button mFalseBtn;
+    TextView mQuestionTV;
+    TextView mScoreTV;
+    ProgressBar mProgressBar;
+    int mScore;
+    int mQuestion;
+    int mIndex;
+
+
+    // TODO: Uncomment to create question bank
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
+
+    // TODO: Declare constants here
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0 / mQuestionBank.length);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            mScore = savedInstanceState.getInt("ScoreKey");
+            mIndex = savedInstanceState.getInt("IndexKey");
+        }
+        else{
+            mScore = 0;
+            mIndex = 0;
+        }
+
+        mTrueBtn = findViewById(R.id.true_btn);
+        mFalseBtn = findViewById(R.id.false_btn);
+        mQuestionTV = findViewById(R.id.question_tv);
+
+        mScoreTV = findViewById(R.id.score);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        mQuestion = mQuestionBank[mIndex].getmQuestionID();
+        mQuestionTV.setText(mQuestion);
+        mScoreTV.setText("Score " + mScore + "/" + mQuestionBank.length);
+
+        mTrueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+        mFalseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion(){
+        mIndex = (mIndex + 1) % mQuestionBank.length;
+
+        if (mIndex == 0){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("That's All Folks!");
+            alert.setCancelable(false);
+            alert.setMessage("Your score is " + mScore + " points");
+            alert.setPositiveButton("Close App", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
+        mQuestion = mQuestionBank[mIndex].getmQuestionID();
+        mQuestionTV.setText(mQuestion);
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+        mScoreTV.setText("Score " + mScore + "/" + mQuestionBank.length);
+    }
+
+    private void checkAnswer(boolean userSelection){
+        boolean correctAnswer = mQuestionBank[mIndex].ismAnswer();
+
+        if (userSelection == correctAnswer){
+            Toast.makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            mScore = mScore + 1;
+        }
+        else {
+            Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("ScoreKey", mScore);
+        outState.putInt("IndexKey", mIndex);
+    }
+}
